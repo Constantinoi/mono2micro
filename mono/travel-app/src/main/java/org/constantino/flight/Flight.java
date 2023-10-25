fcac711c936eae7557b3dc771e23158e83c4781c
Constantino Barreto <pescaalma@gmail.com>
2023-10-24 21:46:13 -0300

    env:Adicionando Flight

---
+++ mono/travel-app/src/main/java/org/constantino/flight/Flight.java
@@ -0,0 +1,13 @@
+package org.constantino.flight;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Entity;
+
+@Entity
+public class Flight extends PanacheEntity {
+
+    public Long travelOrderId;
+    public String fromAirport;
+    public String toAirport;
+
+}
---
+++ mono/travel-app/src/main/java/org/constantino/flight/FlightResource.java
@@ -0,0 +1,35 @@
+package org.constantino.flight;
+
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+
+import java.util.List;
+
+@Path("flight")
+public class FlightResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Flight> flights() {
+        return Flight.listAll();
+    }
+
+    @GET
+    @Path("id")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Flight findById(@QueryParam("id") Long id) {
+        return Flight.findById(id);
+    }
+
+    @POST
+    @Transactional
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Flight newFlight(Flight flight) {
+        flight.id = null;
+        flight.persist();
+        return flight;
+    }
+
+}
