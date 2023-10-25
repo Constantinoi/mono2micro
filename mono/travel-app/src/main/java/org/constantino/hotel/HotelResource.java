77b69400d8c5b9bd96460bbb9d46f9b5f60a2312
Constantino Barreto <pescaalma@gmail.com>
2023-10-24 21:46:22 -0300

    env:Adicionando Hotel

---
+++ mono/travel-app/src/main/java/org/constantino/hotel/HotelResource.java
@@ -0,0 +1,37 @@
+package org.constantino.hotel;
+
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import org.constantino.travelorder.TravelOrder;
+
+import java.util.List;
+
+@Path("hotel")
+public class HotelResource {
+
+    @Produces(MediaType.APPLICATION_JSON)
+    @GET
+    //localhost:8080/hotel/
+    public List<Hotel> hotel() {
+        return Hotel.listAll();
+    }
+
+    @GET
+    @Path("id")
+    //localhost:8080/hotel/id?id=1
+    public Hotel findById(@QueryParam("id") long id) {
+        return Hotel.findById(id);
+    }
+
+    @Transactional//deve nota com transacional para persist
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces({MediaType.APPLICATION_JSON})
+    //curl -d "{}" -H "Content-Type: application/json" localhost:8080/hotel
+    public Hotel newHotel(Hotel hotel) {
+        hotel.id = null;
+        hotel.persist();
+        return hotel;
+    }
+}
